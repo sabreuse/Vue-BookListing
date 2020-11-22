9c71dd71e1e41866725b099ed4668461d3822bca
Amy Hendrix <sabreuse@gmail.com>
2020-11-22 12:14:44 -0500

    connect book form

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,16 +2,21 @@
   <div>
     <h1>{{ title }}</h1>
     <ul>
-      <book-item v-for='book in books' :book='book'></book-item>
+      <book-item v-for='book in books' :book='book' :key='book.index'></book-item>
     </ul>
+    <div>
+      <book-form @addBook='appendBook'></book-form>
+    </div>
   </div>
 </template>
 
 <script>
   import BookItem from './BookItem';
+  import BookForm from './BookForm';
 
   export default {
     name: 'BookList',
+    components: { BookItem, BookForm },
     data() {
       return {
         title: 'All Books',
@@ -22,7 +27,11 @@
                ]
       }
     },
-    components: { BookItem, }
+    methods: {
+      appendBook(bookTitle, bookAuthor){
+        this.books.push({title: bookTitle, author: bookAuthor})
+      }
+    }
   }
 </script>
 
